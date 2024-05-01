c2cdf4c031c124b93dcc48e3cc5f1a8d94de3822
StuW44 <154647503+StuW44@users.noreply.github.com>
2024-04-30 21:11:18 -0400

    Tests

--- starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
+++ starter_code/src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SareetaApplicationTests {
-	static Logger log = Logger.getLogger(SareetaApplicationTests.class);
+
 	@Test
 	public void contextLoads() {
 	}
